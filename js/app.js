eb5d75ac7af8180619dbb62b7a64b7a28599b3c2
Zakaria6789 <jakaria.hossain6363@gmail.com>
2022-03-01 19:52:13 +0600

    add optional chaining

--- js/app.js
+++ js/app.js
@@ -39,11 +39,15 @@ const allphones = (phones) => {
             div.classList.add = 'col';
             div.innerHTML = `
                 <div class="card h-100">
-                    <div class="card-body">
-                    <img src="${phone.image}" class="card-img-top mb-3 w-75" alt="...">
-                        <h5 class="card-title">${phone.phone_name}</h5>
-                        <p class="card-text">${phone.brand}</p>
-                        <button onclick="phoneDetailsDataLoad('${phone.slug}')" class="btn btn-sm btn-primary">Show Details</button>
+                    <div class="card-body text-center">
+                        <div>
+                            <img src="${phone?.image}" class="card-img-top mb-3 w-75" alt="...">
+                        </div>
+                        <div>
+                            <h5 class="card-title">${phone?.phone_name}</h5>
+                            <p class="card-text">${phone?.brand}</p>
+                            <button onclick="phoneDetailsDataLoad('${phone?.slug}')" class="btn btn-sm btn-primary">Show Details</button>
+                        </div>
                     </div>
                 </div>
         `;
@@ -72,21 +76,21 @@ const phoneDetail = (phone) => {
     const div = document.createElement('div');
     div.innerHTML = `
          <div class="text-center mb-5">
-            <img width="280px" src="${phone.image}" alt="">
+            <img width="280px" src="${phone?.image}" alt="">
          </div>
-         <table class="table table-striped w-75 mx-auto border">
+         <table class="table table-striped w-50 mx-auto border">
             <tbody>
                 <tr>
                     <td>Name</td>
-                    <td>${phone.name}</td>
+                    <td>${phone?.name ? phone?.name : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>Brand</td>
-                    <td>${phone.brand}</td>
+                    <td>${phone?.brand ? phone?.brand : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>Release Date</td>
-                    <td>${phone.releaseDate}</td>
+                    <td>${phone.releaseDate ? phone.releaseDate : 'Release Date Not Found'}</td>
                 </tr>
                 <tr>
                     <td class="h4">Main Features</td>
@@ -94,47 +98,47 @@ const phoneDetail = (phone) => {
                 </tr>
                 <tr>
                     <td>ChipSet</td>
-                    <td>${phone.mainFeatures.chipSet}</td>
+                    <td>${phone?.mainFeatures?.chipSet ? phone?.mainFeatures?.chipSet : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>Display Size</td>
-                    <td>${phone.mainFeatures.displaySize}</td>
+                    <td>${phone?.mainFeatures?.displaySize ? phone?.mainFeatures?.displaySize : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>Memory</td>
-                    <td>${phone.mainFeatures.memory}</td>
+                    <td>${phone?.mainFeatures?.memory ? phone?.mainFeatures?.memory : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>Sensors</td>
-                    <td>${phone.mainFeatures.sensors}</td>
+                    <td>${phone?.mainFeatures?.sensors ? phone?.mainFeatures?.sensors : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>Storage</td>
-                    <td>${phone.mainFeatures.storage}</td>
+                    <td>${phone?.mainFeatures?.storage ? phone?.mainFeatures?.storage : 'Not Found'}</td>
                 </tr>
                 <tr>
-                    <td class="h4">Others</td>
+                    <td class="h4">Others</td>'Not Found'
                     <td></td>
                 </tr>
                 <tr>
                     <td>Bluetooth</td>
-                    <td>${phone.others.Bluetooth}</td>
+                    <td>${phone?.others?.Bluetooth ? phone?.others?.Bluetooth : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>GPS</td>
-                    <td>${phone.others.GPS}</td>
+                    <td>${phone?.others?.GPS ? phone?.others?.GPS : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>Radio</td>
-                    <td>${phone.others.Radio}</td>
+                    <td>${phone?.others?.Radio ? phone?.others?.Radio : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>USB</td>
-                    <td>${phone.others.USB}</td>
+                    <td>${phone?.others?.USB ? phone?.others?.USB : 'Not Found'}</td>
                 </tr>
                 <tr>
                     <td>WLAN</td>
-                    <td>${phone.others.WLAN}</td>
+                    <td>${phone?.others?.WLAN ? phone?.others?.WLAN : 'Not Found'}</td>
                 </tr>
             </tbody>
         </table>   
